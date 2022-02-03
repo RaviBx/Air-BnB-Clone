3d92ebb1e471667f34a63a8034f07d315ac2aa1b
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-03 19:57:38 +0530

    Added NavBar Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import reactLogo from "../images/logo.png";
+
+
+
+
+
+export default function Navbar() {
+
+
+    return(
+
+                <nav>
+                    <img src={reactLogo} alt='React Logo'/> 
+
+                </nav>
+
+              
+    );
+}
+
